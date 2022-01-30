8474521f83dd0081a4e7b9597325637554b9a8ed
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-01-30 09:16:26 +0530

    initial commit

---
+++ src/main/java/com/sampleprojects/ecommercestore/springcloudapigateway/SpringcloudapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sampleprojects.ecommercestore.springcloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudapigatewayApplication.class, args);
+	}
+
+}
