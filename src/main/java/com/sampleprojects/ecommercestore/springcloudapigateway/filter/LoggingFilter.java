3ba7323b0e6f49b636a611af96bf4b1ca792f711
krishnakumar.m <krishnakumar.m@endurance.com>
2022-02-20 13:06:38 +0530

    added logging global filter

---
+++ src/main/java/com/sampleprojects/ecommercestore/springcloudapigateway/filter/LoggingFilter.java
@@ -0,0 +1,22 @@
+package com.sampleprojects.ecommercestore.springcloudapigateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter implements GlobalFilter {
+
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+    log.info("Request path received: {}", exchange.getRequest().getPath());
+
+    return chain.filter(exchange);
+  }
+}
